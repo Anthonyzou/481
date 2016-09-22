14050f20a91529a68d3d10aa2b58cfcf662406b1
Anthony Ou <ou.anthony@gmail.com>
2016-09-21 22:46:16 -0600

    updated timer code

--- asn1/main.cpp
+++ asn1/main.cpp
@@ -3,22 +3,35 @@
 #include <random>
 #include <algorithm>
 #include <unistd.h>
+#include <mutex>
+#include <sstream>
 #include "main.h"
 
 using namespace std;
 
-const int numElements = 200;
+const int numElements = 2;
 const int seed = 42;
 const long PROCESSORS = sysconf(_SC_NPROCESSORS_ONLN);
 const long totalElements = PROCESSORS*numElements;
 auto randomArr = randomArray(totalElements);
 
+std::mutex mtx;
+
 
 void call_from_thread(const int from, const int end){
+    mtx.lock();
+    static int tid = 0;
+    cout << tid << endl;
+    tid++;
+    mtx.unlock();
+
     std::sort(&randomArr[from], &randomArr[end]);
+    std::stringstream msg;
+
     for(int i = from; i < end; i++){
-        std::cout << randomArr[i] << std::endl;
+        msg << randomArr[i] << "\n";
     }
+    cout << msg.str();
 }
 
 std::vector<unsigned long> randomArray(long size){
@@ -33,17 +46,17 @@ std::vector<unsigned long> randomArray(long size){
 int main() {
     std::thread threads[PROCESSORS];
     cout << "NUM processors " << PROCESSORS << endl;
-    time_t start, end;
-    time(&start);
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
     for(int i = 0; i < PROCESSORS; i++) {
-        threads[i] = std::thread(&call_from_thread, i*numElements, (i+1)*numElements-1);
+        threads[i] = std::thread(&call_from_thread, i*numElements, (i+1)*numElements);
     }
 
     for(int it = 0; it < PROCESSORS; it++) {
         threads[it].join();
     }
 
-    time(&end);
-    std::cout << difftime(end, start) << " seconds" << std::endl;
+    std::chrono::steady_clock::time_point end= std::chrono::steady_clock::now();
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() <<std::endl;
     return 0;
 }
