8b4a197c77188cea99df1e787b1a28c569d7b191
Anthony Ou <ou.anthony@gmail.com>
2016-09-21 22:02:02 -0600

    Using sort function

--- asn1/main.cpp
+++ asn1/main.cpp
@@ -1,48 +1,49 @@
 #include <iostream>
 #include <thread>
-#include <cstdlib>
-#include <ctime>
-#include <vector>
-#include <stdlib.h>
+#include <random>
+#include <algorithm>
 #include <unistd.h>
+#include "main.h"
 
 using namespace std;
 
+const int numElements = 200;
+const int seed = 42;
+const long PROCESSORS = sysconf(_SC_NPROCESSORS_ONLN);
+const long totalElements = PROCESSORS*numElements;
+auto randomArr = randomArray(totalElements);
 
 
-std::vector<int> randomArray(int size);
-void call_from_thread(int * i);
-void call_from_thread(int * i){
-    auto it = (*i).begin();
-    while (it != (*i).end()){
-        std::cout << *it << std::endl;
-        it++;
+void call_from_thread(const int from, const int end){
+    std::sort(&randomArr[from], &randomArr[end]);
+    for(int i = from; i < end; i++){
+        std::cout << randomArr[i] << std::endl;
     }
 }
 
-std::vector<int> randomArray(int size){
-    std::vector<int> vec;
+std::vector<unsigned long> randomArray(long size){
+    auto a = default_random_engine(seed);
+    std::vector<unsigned long> vec;
     for(int i = 0; i < size; i++) {
-        vec.push_back(i);
+        vec.push_back(a());
     }
     return vec;
 }
 
-const long PROCESSORS = sysconf(_SC_NPROCESSORS_ONLN);
-
 int main() {
     std::thread threads[PROCESSORS];
-    cout << PROCESSORS << endl;
-
-    auto ki = randomArray(20);
+    cout << "NUM processors " << PROCESSORS << endl;
+    time_t start, end;
+    time(&start);
     for(int i = 0; i < PROCESSORS; i++) {
-        threads[i] = std::thread(&call_from_thread, &ki);
-
+        threads[i] = std::thread(&call_from_thread, i*numElements, (i+1)*numElements-1);
     }
 
-    for(long it = 0; it < PROCESSORS; it++) {
+    for(int it = 0; it < PROCESSORS; it++) {
         threads[it].join();
     }
 
+    time(&end);
+    std::cout << difftime(end, start) << " seconds" << std::endl;
     return 0;
 }
---
+++ asn1/main.h
@@ -0,0 +1,4 @@
+#pragma once
+
+std::vector<unsigned long> randomArray(long size);
+void call_from_thread(const int from, const int end);
