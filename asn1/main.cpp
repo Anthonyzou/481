2e07c27f09373d0b4f4e75a00b48148fe0c545f4
Anthony Ou <ou.anthony@gmail.com>
2016-09-27 23:51:06 -0600

    Fixed random generated arrays

--- asn1/main.cpp
+++ asn1/main.cpp
@@ -6,6 +6,7 @@
 #include <sstream>
 #include <future>
 #include "main.h"
+#include <algorithm>
 
 using namespace std;
 
@@ -15,21 +16,21 @@ inline void handleChunk(int idx, vec results){
     phase4[idx].insert(phase4[idx].end(), results.begin(), results.end());
     p4v[idx].unlock();
     threadsDone[idx]++;
-    cv[idx].notify_one();
+    p4CV[idx].notify_one();
 }
 
 void worker(const int id, promise<vec> prom, const int from, const int end){
     //PHASE 1
     std::sort(&randomArr[from], &randomArr[end]);
     vec phase1Arr;
-    for(auto i = from; i < end; i+=sampleIntervals){
+    for(auto i = from; i < end; i += sampleIntervals){
         phase1Arr.push_back(randomArr[i]);
     }
     prom.set_value(phase1Arr);
 
     //PHASE 3
-    auto pivots = phase2Vector.get();
     auto idx = 0;
+    auto pivots = phase2Vector.get();
     auto pivot = pivots[idx];
     vec results;
     for(auto i = from; i < end; i++){
@@ -44,19 +45,21 @@ void worker(const int id, promise<vec> prom, const int from, const int end){
     handleChunk(idx, results);
 
     std::unique_lock<std::mutex> lk(p4[id]);
-    cv[id].wait(lk, [id](){return threadsDone[id] == PROCESSORS;});
+    p4CV[id].wait(lk, [id](){return threadsDone[id] == PROCESSORS;});
 
     //PHASE 4 SORT
-    sort(phase4[id].begin(), phase4[id].end());
+    merge_sort(phase4[id].begin(), phase4[id].end());
 }
 
 int main() {
     vector<std::thread> threads;
     vector<future<vec>> phase1Results;
+    pthread_setconcurrency(PROCESSORS);
+    vec results, subResults, phase4Results;
 
     cout << "NUM processors " << thread::hardware_concurrency() << endl;
-    auto begin = std::chrono::steady_clock::now();
 
+    auto begin = std::chrono::steady_clock::now();
     //CREATE THREADS
     for(int i = 0; i < PROCESSORS; i++) {
         promise<vec> phase1Prom;
@@ -66,7 +69,6 @@ int main() {
     }
 
     //PHASE 2
-    vec results, subResults;
     for(auto &result: phase1Results){
         auto subArrays = result.get();
         results.insert(results.end(), subArrays.begin(), subArrays.end());
@@ -74,7 +76,8 @@ int main() {
     sort(results.begin(), results.end());
 
     //PHASE 2 GET PIVOT POINTS
-    for(auto i = PROCESSORS; i < results.size(); i += PROCESSORS)
+    auto k = 0;
+    for(auto i = PROCESSORS; k++ < PROCESSORS-1; i += PROCESSORS)
         subResults.push_back(results[i]);
 
     phase2Promise.set_value(subResults);
@@ -84,12 +87,19 @@ int main() {
     auto end = chrono::steady_clock::now();
 
     //COMBINE RESULTS FROM PHASE 4
-    for(auto &id : phase4)
-        for(auto &t : id)
-            cout << t << " " ;
+    stringstream s;
+    for(auto &id : phase4){
+        phase4Results.insert(phase4Results.end(), id.begin(), id.end());
+//        for(auto &t : id){
+//
+//            s << t << " " ;
+//        }
+    }
 
-    cout << endl;
+//    s << endl;
+    s << is_sorted(phase4Results.begin(), phase4Results.end()) << endl;
 
-    std::cout << "Time difference = " << chrono::duration_cast<chrono::microseconds>(end - begin).count() << std::endl;
+    s << "Time difference = " << chrono::duration_cast<chrono::microseconds>(end - begin).count() << std::endl;
+    cout << s.str();
     return 0;
 }
--- asn1/main.h
+++ asn1/main.h
@@ -3,6 +3,9 @@
 #include <iterator>
 #include <algorithm>
 #include <functional>
+#include <stdlib.h>
+#include <cstdlib>
+#include <cmath>
 
 using namespace std;
 
@@ -11,35 +14,35 @@ typedef vector<unsigned long> vec;
 
 vec randomArray(long size);
 void worker(const int id, promise<vec> prom, const int from, const int end);
+inline void handleChunk(int idx, vec results);
+
 //thread::hardware_concurrency()
 
-const int numElements = 36/3;
+const int numElements = 2000000;
 const int seed = 42;
-const long PROCESSORS = 3;
-const long totalElements = 36;
+const auto PROCESSORS = thread::hardware_concurrency();
+const long totalElements = PROCESSORS*numElements;
 const auto sampleIntervals = totalElements/(PROCESSORS*PROCESSORS);
 
 promise<vec> phase2Promise; shared_future<vec> phase2Vector(phase2Promise.get_future());
 
 vector<vec> phase4(PROCESSORS);
 vector<mutex> p4(PROCESSORS), p4v(PROCESSORS);
-vector<condition_variable> cv(PROCESSORS);
+vector<condition_variable> p4CV(PROCESSORS);
 vector<int> threadsDone(PROCESSORS, 0) ;
 
 vec randomArray(long size){
-    auto a = default_random_engine(seed);
-    vec v;
-    for(int i = 0; i < size; i++) {
-        v.push_back(a());
-    }
+//    auto a = default_random_engine(seed);
+    vec v((unsigned long) size);
+    std::srand(seed);
+    for(int i = 0; i < size; i++)
+        v[i]= (std::rand());
     return v;
 }
 
-vec randomArr = {
-        16,2,17,24,33,28,30,1,0,27,9,25,34,23,19,18,11,7,
-        21,13,8,35,12,29,6,3,4,14,22,15,32,10,26,31,20,5
-};
+vec randomArr = randomArray(totalElements);
 
+// https://en.wikibooks.org/wiki/Algorithm_Implementation/Sorting/Merge_sort
 template <typename BidirIt, typename Compare = std::less<unsigned long>>
 void merge_sort(BidirIt first, BidirIt last, Compare cmp = Compare {})
 {
