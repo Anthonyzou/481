02e21bc6b032b37e050ec10d331f66a2ab5bf125
Anthony Ou <ou.anthony@gmail.com>
2016-10-04 02:03:54 -0600

    Added std and axis

--- asn1/benchmark.sh
+++ asn1/benchmark.sh
@@ -4,36 +4,36 @@ seq=$(seq 200000 100000 1000000)
 rm seq.txt *.psrs
 numProcessors=$(($(nproc)+2))
 echo ${seq}
-for processor in $(seq 2 1 ${numProcessors})
-do
-    file="${processor}.psrs"
-    echo "["  >> ${file}
-    for elements in ${seq}
-    do
-        printf "["  >> ${file}
-        for i in $(seq 5)
-        do
-            echo "[$(./asn1 -threads ${processor} -size ${elements})]," >> ${file}
-        done
-        printf "],"  >> ${file}
-
-    done
-    echo "]"  >> ${file}
-done
+#for processor in $(seq 2 1 ${numProcessors})
+#do
+#    file="${processor}.psrs"
+#    echo "["  >> ${file}
+#    for elements in ${seq}
+#    do
+#        printf "["  >> ${file}
+#        for i in $(seq 5)
+#        do
+#            echo "[$(./asn1 -threads ${processor} -size ${elements})]," >> ${file}
+#        done
+#        printf "],"  >> ${file}
+#
+#    done
+#    echo "]"  >> ${file}
+#done
 
 
 file="seq.txt"
+printf "[" >> seq.txt
 for elements in ${seq}
 do
-    printf ${elements} >> ${file}
-    printf ":[" >> seq.txt
+    printf "[" >> seq.txt
     for i in $(seq 5)
     do
         printf $(./asn1test)"," >> ${file}
     done
-    echo "]" >> ${file}
-    echo "" >> ${file}
+    echo "]," >> ${file}
 done
+echo "]" >> ${file}
 
 
 
--- asn1/release/graph.py
+++ asn1/release/graph.py
@@ -1,12 +1,24 @@
 
 import numpy as np
 import ast
+import glob, os
 
-with open('./2.psrs') as f:
-    my_lines = ast.literal_eval(f.read())
+seq = [200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000]
 
-    for i in (my_lines):
+for file in glob.glob("*.psrs"):
+    print(file)
+    with open(file) as f:
+        for i in ast.literal_eval(f.read()):
+            a = np.matrix(i)
+            b=a.sum(axis=0)
+            c=[int(x) for x in a.std(axis=0).tolist()[0]]
+            print(c)
+            print(b//5)
+
+with open("seq.txt") as f:
+    for i in ast.literal_eval(f.read()):
+        print(i)
         a = np.matrix(i)
-        # print(a)
-        a=a.sum(axis=0)
-        print(a//5)
+        b=a.sum(axis=0)
+        print( a.std(axis=1))
+        print(b//5)
