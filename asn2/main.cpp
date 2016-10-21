3c08bfc97d68dbf18624944631a7d45646257e01
Anthony Ou <ou.anthony@gmail.com>
2016-10-21 15:01:26 -0600

    Added boilerplate

--- asn2/main.cpp
+++ asn2/main.cpp
@@ -1,13 +1,30 @@
-#include <boost/mpi/environment.hpp>
-#include <boost/mpi/communicator.hpp>
-#include <iostream>
-namespace mpi = boost::mpi;
+#include "main.h"
 
-int main(int argc, char ** argv)
+#include <boost/mpi.hpp>
+#include <iostream>
+#include <string>
+#include <boost/serialization/string.hpp>
+namespace mpi = boost::mpi;
+using namespace mpi;
+
+int main(int argc, char* argv[])
 {
-    mpi::environment env(argc, argv);
-    mpi::communicator world;
-    std::cout << "I am process " << world.rank() << " of " << world.size()
-              << "." << std::endl;
+    environment env(argc, argv);
+    communicator world;
+    if (world.rank() == 0) {
+        request reqs[2];
+        std::string msg, out_msg = "Hello";
+        reqs[0] = world.isend(1, 0, out_msg);
+        reqs[1] = world.irecv(1, 1, msg);
+        wait_all(reqs, reqs + 2);
+        std::cout << msg << "!" << std::endl;
+    } else {
+        request reqs[2];
+        std::string msg, out_msg = "world";
+        reqs[0] = world.isend(0, 1, out_msg);
+        reqs[1] = world.irecv(0, 0, msg);
+        wait_all(reqs, reqs + 2);
+        std::cout << msg << ", ";
+    }
     return 0;
 }
