5a2b80957117d866319bd2c6d2678fc1d22320c7
Anthony Ou <ou.anthony@gmail.com>
2016-10-21 19:24:25 -0600

    working phase 1

--- asn2/main.cpp
+++ asn2/main.cpp
@@ -4,42 +4,58 @@
 #include <iostream>
 #include <string>
 #include <boost/serialization/string.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/format.hpp>
+
+#include <stdlib.h>
+
+#include <list>
+
 namespace mpi = boost::mpi;
 using namespace mpi;
+using namespace boost;
+using namespace std;
+
+//    cout << boost::format("writing %1%,  x=%2% : %3%-th try\n") % "toto" % 40.23 % 50;
+// prints "writing toto,  x=40.230 : 50-th try"
 
 int main(int argc, char* argv[])
 {
     auto start = chrono::steady_clock::now();
 
+    init(argc, argv);
     environment env(argc, argv);
     communicator world;
-    if (world.rank() == 0) {
-        request reqs[2];
-        std::string msg, out_msg = "Hello";
-        reqs[0] = world.isend(1, 0, out_msg);
-        reqs[1] = world.irecv(1, 1, msg);
-        wait_all(reqs, reqs + 2);
-        std::cout << msg << "!";
-    } else {
-        request reqs[2];
-        std::string msg, out_msg = "world";
-        reqs[0] = world.isend(0, 1, out_msg);
-        reqs[1] = world.irecv(0, 0, msg);
-        wait_all(reqs, reqs + 2);
-        std::cout << msg << ", ";
-    }
 
-//    std::cout<< endl;
-    std::string value;
+    vec mainArray;
+    int perProcess = 3;
+    totalElements = perProcess * world.size();
 
     if (world.rank() == 0) {
-        value = "Hello, World!";
+        mainArray = randomArray(totalElements);
+    }
+    broadcast(world, mainArray, 0);
+
+    auto begin = mainArray.begin()+world.rank()*perProcess;
+    auto end = begin+perProcess;
+    sort(begin, end);
+
+    if (world.rank() == 0) {
+        vector<vec> all_numbers;
+        gather(world, mainArray, all_numbers, 0);
+        stringstream s;
+        for (int proc = 0; proc < world.size(); ++proc) {
+            for(auto &i : all_numbers[proc]){
+                s << i << endl;
+            }
+            s << endl;
+        }
+        cout << s.str();
+    }
+    else {
+        gather(world, mainArray, 0);
     }
 
-    broadcast(world, value, 0);
-
-    std::cout << "Process #" << world.rank() << " says " << value
-              << std::endl;
 
     return 0;
 }
