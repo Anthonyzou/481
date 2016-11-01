42248ae3dbcf19713112dd17cd8d9bd7a6e3981c
Anthony Ou <ou.anthony@gmail.com>
2016-10-31 23:50:29 -0600

    Updated graphing

--- asn2/release/graph.py
+++ asn2/release/graph.py
@@ -16,6 +16,15 @@ with open("seq.txt") as f:
         a = np.matrix(i)
         b=int(np.average(a))/1000000
         seq.append(b)
+print(seq)
+
+plt.figure()
+plt.plot (size, seq )
+
+plt.xlabel('Elements')
+plt.ylabel('Runtime (seconds)')
+plt.title('Runtime of sequential merge sort')
+plt.savefig("figure1.png")
 
 for file in sorted(glob.glob("*.psrs")):
     with open(file) as f:
@@ -33,15 +42,7 @@ for file in sorted(glob.glob("*.psrs")):
         psrserr.append(phaseserr)
         totalsPSRS.append(totals)
 
-# print(seq)
-
-# plt.plot (size, seq )
-#
-# plt.xlabel('Elements')
-# plt.ylabel('Runtime (seconds)')
-# plt.title('Runtime of sequential merge sort')
-# plt.savefig("figure1.png")
-
+plt.figure()
 err = (np.matrix(phaseserr).transpose())
 print(err)
 speedup = (seq/np.matrix(totalsPSRS)).transpose()
@@ -53,20 +54,20 @@ plt.xlabel('Processors')
 plt.ylabel('Speedup')
 plt.title('Speedup by elements and # threads')
 plt.legend([str(size[i])+' elements'for i in range(0,len(size))]).draggable()
-plt.show()
+plt.savefig("figure2.png")
 
-# bar_width = 0.35
-#
-# phases = ["phase 1","phase 2","phase 3","phase 4",]
-# tmp = psrs[2][3][:-1]
-# plt.bar(np.arange(4)-bar_width,tmp, bar_width, align='center', label="3 Threads", color='r', yerr=psrserr[2][3][:-1])
-# tmp = psrs[3][3][:-1]
-# plt.bar(np.arange(4),tmp, bar_width, align='center', label="4 Threads", color='b', yerr=psrserr[3][3][:-1])
-# tmp = psrs[4][3][:-1]
-# plt.bar(np.arange(4)+bar_width,tmp, bar_width, align='center', label="5 Threads", color='g', yerr=psrserr[4][3][:-1])
-# plt.legend().draggable()
-# plt.xticks(range(4), phases)
-# plt.xlabel('phases')
-# plt.ylabel('time spent')
-# plt.title('Time spent in seconds on each phase for 11 million elements')
-# plt.savefig("figure2.png")
+plt.figure()
+bar_width = 0.35
+phases = ["phase 1","phase 2","phase 3","phase 4",]
+tmp = psrs[2][3][:-1]
+plt.bar(np.arange(4)-bar_width,tmp, bar_width, align='center', label="3 Threads", color='r', yerr=psrserr[2][3][:-1])
+tmp = psrs[3][3][:-1]
+plt.bar(np.arange(4),tmp, bar_width, align='center', label="4 Threads", color='b', yerr=psrserr[3][3][:-1])
+tmp = psrs[4][3][:-1]
+plt.bar(np.arange(4)+bar_width,tmp, bar_width, align='center', label="5 Threads", color='g', yerr=psrserr[4][3][:-1])
+plt.legend().draggable()
+plt.xticks(range(4), phases)
+plt.xlabel('phases')
+plt.ylabel('time spent')
+plt.title('Time spent in seconds on each phase for 11 million elements')
+plt.savefig("figure3.png")
